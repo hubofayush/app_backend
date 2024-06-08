372283400a58b96b2d8d4506ac8c61663f9cedd6
Ayush Amberkar <ayushempire03@gmail.com>
2024-06-08 21:44:08 +0530

    created ApiResponce file

---
+++ src/utils/ApiResponce.js
@@ -0,0 +1,9 @@
+class ApiResponce {
+    constructor(statuscode, data, message = "success") {
+        this.statuscode = statuscode;
+        this.data = data;
+        this.message = message;
+        this.success = statuscode < 400;
+    }
+}
+export { ApiResponce };
