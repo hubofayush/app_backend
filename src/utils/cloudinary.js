492df43b57c97bf528082929011914208df0587d
Ayush Amberkar <ayushempire03@gmail.com>
2024-06-22 13:04:41 +0530

    created upload function to upload files on cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,3 +7,21 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        // ** upload file on cloudinary
+        const responce = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+        });
+        //file has been uploaded successfully
+        console.log("file uploaded on cloudinary", responce.url);
+        return responce; // for user
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // will remove local saved file if upload file failed
+        return null;
+    }
+};
+
+export { uploadOnCloudinary };
