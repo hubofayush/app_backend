e79079bff89b2db14165d6fc9928cb87e6871db4
Ayush Amberkar <ayushempire03@gmail.com>
2024-06-23 22:11:36 +0530

    used multer middleware in post method in register route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,23 @@ import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
 const router = Router();
 
-router.route("/register").post(registerUser);
+// impoting upload from multer
+import { upload } from "../middlewares/multer.middleware.js";
+
+router.route("/register").post(
+    // middleware //
+    upload.feilds([
+        {
+            name: "avatar",
+            maxCount: 1,
+        },
+        {
+            name: "coverImage",
+            maxCount: 1,
+        },
+    ]),
+
+    registerUser, // controller
+);
 
 export default router;
