41e3829931544e8506c9dd9297da2b26ba531f16
Ayush Amberkar <ayushempire03@gmail.com>
2024-06-23 14:28:03 +0530

    created a controller and route for user

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,11 @@
+// importing async handler.js
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const 
+registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({
+        message: "ok",
+    });
+});
+
+export { registerUser };
