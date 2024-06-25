8ad3a8e57902b54df5b985aa1a8d7ea1eea82b5e
Ayush Amberkar <ayushempire03@gmail.com>
2024-06-26 01:00:27 +0530

    auth midlleare for multiple use and to check user logedin or not by access token

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,41 @@
+import { User } from "../models/user.model";
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    try {
+        // getting cookies
+        const token =
+            req.cookies?.accessToken ||
+            req.header("Authorization")?.replace("bearer", "");
+
+        if (!token) {
+            new ApiError(401, "Unauthorized access");
+        }
+
+        // verifying access token using jwt //
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        // end of verifying access token using jwt //
+
+        // finding user using this token id //
+        const user = await User.findById(decodedToken?._id).select(
+            "-password -refreshToken",
+        );
+        // end of finding user using this token id //
+
+        // checking user is fetched or not //
+        if (!user) {
+            // frontend //
+            throw new ApiError(401, "Invalid AccessToken");
+        }
+        // end of checking user is fetched or not //
+
+        // adding new object in req //
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401,error?.message || 'invalid access token')
+    }
+    // end of adding new object in req //
+});
