d0c0c8e306d371c5a64ea2f4867cc7dc757e13bd
Ayush Amberkar <ayushempire03@gmail.com>
2024-06-25 11:07:05 +0530

    avoid error of username varible correctly referenced at token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -78,7 +78,7 @@ UserSchema.methods.generateAccessToken = function () {
         {
             _id: this._id,
             email: this.email,
-            username: this.userName,
+            username: this.username,
             fullName: this.fullName,
         },
         process.env.ACCESS_TOKEN_SECRET,
