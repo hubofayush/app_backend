23b59650831d3a95e12881abe8a6363aa27fb55d
Ayush Amberkar <ayushempire03@gmail.com>
2024-06-26 01:01:21 +0530

    create route for logout and login

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+    loginUser,
+    logoutUser,
+    registerUser,
+} from "../controllers/user.controller.js";
 const router = Router();
 
 // impoting upload from multer
 import { upload } from "../middlewares/multer.middleware.js";
 
+// importing verifyJwt from auth middlware
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+// register router //
 router.route("/register").post(
     // middleware //
     upload.fields([
@@ -20,5 +28,15 @@ router.route("/register").post(
 
     registerUser, // controller
 );
+
+// end of register router //
+
+// login user route //
+router.route("/login").post(loginUser);
+// end of login user route //
+
+// secured routes //
+router.route("/logout").post(verifyJWT, logoutUser);
+// end of secured routes //
 
 export default router;
