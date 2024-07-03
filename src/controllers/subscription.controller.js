9830a9fefcc0dff671271919e3610e7c3addb5e4
Ayush Amberkar <ayushempire03@gmail.com>
2024-07-03 17:02:21 +0530

    add get channel subcribers controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponce } from "../utils/ApiResponce.js";
 import { Subscription } from "../models/subscription.model.js";
+import mongoose from "mongoose";
 
 // toggle subscription //
 const toggelSubscription = asyncHandler(async (req, res) => {
@@ -41,4 +42,65 @@ const toggelSubscription = asyncHandler(async (req, res) => {
 });
 // end of toggle subscription //
 
-export { toggelSubscription };
+// get subscrbers list of channel//
+const getChannelSubscribers = asyncHandler(async (req, res) => {
+    const { channelId } = req.params;
+
+    if (!channelId) {
+        throw new ApiError(404, "channel id required");
+    }
+
+    const channelSubscribers = await Subscription.aggregate([
+        {
+            $match: {
+                channel: new mongoose.Types.ObjectId(channelId),
+            },
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "subscriber",
+                foreignField: "_id",
+                as: "subscribers",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            fullName: 1,
+                            avatar: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $addFields: {
+                subscribers: {
+                    $first: "$subscribers",
+                },
+            },
+        },
+        {
+            $project: {
+                subscribers: 1,
+            },
+        },
+    ]);
+
+    if (channelSubscribers.length === 0) {
+        throw new ApiError(400, "invalid id");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponce(
+                200,
+                channelSubscribers,
+                "subscribers fetched successfully",
+            ),
+        );
+});
+// end of get subscrbers list of channel//
+
+export { toggelSubscription, getChannelSubscribers };
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,11 +1,15 @@
 import Router from "express";
 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { toggelSubscription } from "../controllers/subscription.controller.js";
+import {
+    getChannelSubscribers,
+    toggelSubscription,
+} from "../controllers/subscription.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
 
 router.route("/toggle-subscription/:channelId").post(toggelSubscription);
 
+router.route("/get-subscribers/:channelId").get(getChannelSubscribers);
 export default router;
