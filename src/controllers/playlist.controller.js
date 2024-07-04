b9e6b7238a77c19a9489aff9e2ec297d1bf235dc
Ayush Amberkar <ayushempire03@gmail.com>
2024-07-04 13:45:48 +0530

    add get user playlists by id controller and route

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponce } from "../utils/ApiResponce.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Playlist } from "../models/playlist.model.js";
-
+import { isValidObjectId } from "mongoose";
 // create playlist //
 const createPlaylist = asyncHandler(async (req, res) => {
     const { name, description } = req.body;
@@ -25,10 +25,33 @@ const createPlaylist = asyncHandler(async (req, res) => {
         .status(200)
         .json(new ApiResponce(200, playlist, "playlist created successfully"));
 });
-
 // end of create playlist //
 
-// get user playlist //
+// get user playlists //
+const getUserPlaylists = asyncHandler(async (req, res) => {
+    const { userId } = req.params;
+
+    if (!userId) {
+        throw new ApiError(404, "user id required");
+    }
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(404, "invalid id");
+    }
+
+    const playlist = await Playlist.find({
+        owner: userId,
+    });
+
+    if (!playlist) {
+        throw new ApiError(400, "cant get playlist or invalid id");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponce(200, playlist, "playlists fetched successfully"));
+});
+// end get user playlists //
 
 // get playlist by id //
 
@@ -40,4 +63,4 @@ const createPlaylist = asyncHandler(async (req, res) => {
 
 // delete playlist //
 
-export { createPlaylist };
+export { createPlaylist, getUserPlaylists };
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,11 +1,15 @@
 import Router from "express";
 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createPlaylist } from "../controllers/playlist.controller.js";
+import {
+    createPlaylist,
+    getUserPlaylists,
+} from "../controllers/playlist.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
 
 router.route("/create-playlist").post(createPlaylist);
+router.route("/user-playlists/:userId").get(getUserPlaylists);
 
 export default router;
